4f3b8a0911f8980b8307ed6882823d9501c8a242
arun-uitoux <arunkumar@uitoux.in>
2020-06-30 20:55:30 +0530

    model import changes

--- src/models/AmcPolicy.php
+++ src/models/AmcPolicy.php
@@ -3,18 +3,30 @@
 namespace Abs\AmcPkg;
 
 use Abs\HelperPkg\Traits\SeederTrait;
+use App\BaseModel;
+// use Illuminate\Database\Eloquent\Model;
 use App\Company;
-use App\Config;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class AmcPolicy extends Model {
+class AmcPolicy extends BaseModel {
 	use SeederTrait;
 	use SoftDeletes;
 	protected $table = 'amc_policies';
+	public static $AUTO_GENERATE_CODE = false;
+
+	protected static $excelColumnRules = [
+		'Name' => [
+			'table_column_name' => 'name',
+			'rules' => [
+				'required' => [
+				],
+			],
+		],
+	];
+
 	public $timestamps = true;
 	protected $fillable =
-		["id","company_id","name"]
+		["id", "company_id", "name"]
 	;
 
 	public function getDateOfJoinAttribute($value) {
@@ -25,7 +37,55 @@ class AmcPolicy extends Model {
 		return $this->attributes['date_of_join'] = empty($date) ? NULL : date('Y-m-d', strtotime($date));
 	}
 
-	public static function createFromObject($record_data) {
+	public static function saveFromObject($record_data) {
+		$record = [
+			'Company Code' => $record_data->company_code,
+			'Name' => $record_data->name,
+		];
+		return static::saveFromExcelArray($record);
+	}
+
+	public static function saveFromExcelArray($record_data) {
+		$errors = [];
+		$company = Company::where('code', $record_data['Company Code'])->first();
+		if (!$company) {
+			return [
+				'success' => false,
+				'errors' => ['Invalid Company : ' . $record_data['Company Code']],
+			];
+		}
+
+		if (!isset($record_data['created_by_id'])) {
+			$admin = $company->admin();
+
+			if (!$admin) {
+				return [
+					'success' => false,
+					'errors' => ['Default Admin user not found'],
+				];
+			}
+			$created_by_id = $admin->id;
+		} else {
+			$created_by_id = $record_data['created_by_id'];
+		}
+
+		$record = self::firstOrNew([
+			'company_id' => $company->id,
+			'name' => $record_data['Name'],
+		]);
+
+		$result = Self::validateAndFillExcelColumns($record_data, Static::$excelColumnRules, $record);
+		if (!$result['success']) {
+			return $result;
+		}
+		$record->company_id = $company->id;
+		$record->created_by_id = $created_by_id;
+		$record->save();
+		return [
+			'success' => true,
+		];
+	}
+	/*public static function createFromObject($record_data) {
 
 		$errors = [];
 		$company = Company::where('code', $record_data->company)->first();
@@ -58,7 +118,7 @@ class AmcPolicy extends Model {
 		$record->created_by_id = $admin->id;
 		$record->save();
 		return $record;
-	}
+	}*/
 
 	public static function getList($params = [], $add_default = true, $default_text = 'Select Amc Policy') {
 		$list = Collect(Self::select([
